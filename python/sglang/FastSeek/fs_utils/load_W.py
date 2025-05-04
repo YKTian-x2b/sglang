4cb0a51b644f62ef98163b04a001f8d39b6ace7d
tianyaokai <tianyaokai@bytedance.com>
2025-05-04 16:04:46 +0800

    update

--- python/sglang/FastSeek/fs_utils/load_W.py
+++ python/sglang/FastSeek/fs_utils/load_W.py
@@ -3,9 +3,19 @@ import os
 from safetensors import safe_open
 
 
+def load_keys(in_pth, out_pth):
+    wf = open(out_pth, "a")
+    model = torch.load(in_pth, weights_only=True)
+    with open(out_pth, "w") as wf:
+        wf.write('[')
+        for key, value in model.items():
+            wf.write(f"'{key}', ")
+        wf.write(']')
+
+
 def load_pt(in_pth, out_pth):
     wf = open(out_pth, "a")
-    model = torch.load(in_pth)
+    model = torch.load(in_pth, weights_only=True)
     for key, value in model.items():
         print(f"Key: {key}\nShape: {value.shape}\n", file=wf)
 
@@ -18,7 +28,8 @@ def load_safetensor(in_pth, out_pth):
             
 
 if __name__ == "__main__":
-    in_pth = "/home/private_xgpt_repo/sglang/python/sglang/FastSeek/fs_models/model_fp8_4layer.pt"
-    out_pth = "/home/private_xgpt_repo/sglang/python/sglang/FastSeek/fs_models/dsv3_fp8_state_dict.txt"
-    # load_safetensor(in_pth, out_pth)
+    in_pth = "/home/private_xgpt_repo/sglang/python/sglang/FastSeek/fs_models/r1_w4a8_4layers.pt"
+    out_pth = "/home/private_xgpt_repo/sglang/python/sglang/FastSeek/fs_models/dsv3_w4a8_state_dict.txt"
+    # out_pth = "/home/private_xgpt_repo/sglang/python/sglang/FastSeek/fs_models/dsv3_fp8_keys.txt"
     load_pt(in_pth, out_pth)
+    # load_keys(in_pth, out_pth)
